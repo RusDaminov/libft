3f70501951d4d6156d966c7f190e8ad04b58650f
Abernita <88611385+RusDaminov@users.noreply.github.com>
2022-04-01 16:54:36 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,29 +12,51 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	*str;
+
+// 	if (!s1 || !s2)
+// 		return (0);
+// 	i = 0;
+// 	j = 0;
+// 	while (s1[i])
+// 		i++;
+// 	while (s2[j])
+// 		j++;
+// 	str = (char *) malloc((i + j + 1) * sizeof(char));
+// 	if (str == NULL)
+// 		return (NULL);
+// 	i = -1;
+// 	j = -1;
+// 	while (s1[++i])
+// 		str[i] = s1[i];
+// 	while (s2[++j])
+// 		str[i + j] = s2[j];
+// 	str[i + j] = '\0';
+// 	return (str);
+// }
+
+char	*ftt_strjoin(char *s1, char *s2)
 {
+	char	*res;
 	int		i;
 	int		j;
-	char	*str;
 
 	if (!s1 || !s2)
 		return (0);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	str = (char *) malloc((i + j + 1) * sizeof(char));
-	if (str == NULL)
+	res = malloc(sizeof(*res) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!res)
 		return (NULL);
 	i = -1;
-	j = -1;
 	while (s1[++i])
-		str[i] = s1[i];
+		res[i] = s1[i];
+	j = -1;
 	while (s2[++j])
-		str[i + j] = s2[j];
-	str[i + j] = '\0';
-	return (str);
+		res[i + j] = s2[j];
+	free(s1);
+	res[i + j] = '\0';
+	return (res);
 }
